9b8f66557f1d45afe9b719c2883b017e71341836
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-08-18 15:32:40 -0300

    Create

---
+++ Blog/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Blog.Models;
+using Blog.Data;
+
+namespace Blog
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new BlogDataContext())
+            {
+                var tag = new Tag { Name = "Ast.Net", Slug = "Aspnet.core" };
+                context.Tags.Add(tag);
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+
+
+
+}
