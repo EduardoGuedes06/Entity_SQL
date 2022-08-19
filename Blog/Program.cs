d183da70cad7c12a805af1cd1ea248891f6654c1
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-08-19 12:38:54 -0300

    Introdulção

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -9,22 +9,10 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            using(var context = new BlogDataContext())
-            {
-                //var tag = new Tag { Name = "Ast.Net", Slug = "Aspnet.core" };
-                //context.Tags.Add(tag);
-                //context.SaveChanges();
-                var tag = context.Tags.FirstOrDefault(x => x.Id == 4);
-                tag.Name = "DotNet";
-                tag.Slug = "dotnet.json";
 
-                context.Update(tag);
-                context.SaveChanges();
-            }
         }
 
-
-    }
+    } 
 
 
 
