ce07e785567da1339029c300395a7b86373aaab0
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-08-19 15:34:22 -0300

    Required

--- Blog/Models/Category.cs
+++ Blog/Models/Category.cs
@@ -1,9 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
 namespace Blog.Models
 {
+    [Table("Category")]
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(80)]
+        [MinLength(4)]
+        [Column("Name",TypeName = "VARCHAR")]
         public string? Name { get; set; }
+        [Required]
+        [MaxLength(80)]
+        [MinLength(4)]
+        [Column("Name", TypeName = "VARCHAR")]
         public string? Slug { get; set; }
     }
 }
