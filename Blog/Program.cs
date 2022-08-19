7c7d6f41aa7f05e062cb75539f76852af777fb66
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-08-19 10:45:36 -0300

    Update

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -11,8 +11,14 @@ namespace Blog
         {
             using(var context = new BlogDataContext())
             {
-                var tag = new Tag { Name = "Ast.Net", Slug = "Aspnet.core" };
-                context.Tags.Add(tag);
+                //var tag = new Tag { Name = "Ast.Net", Slug = "Aspnet.core" };
+                //context.Tags.Add(tag);
+                //context.SaveChanges();
+                var tag = context.Tags.FirstOrDefault(x => x.Id == 4);
+                tag.Name = "DotNet";
+                tag.Slug = "dotnet.json";
+
+                context.Update(tag);
                 context.SaveChanges();
             }
         }
